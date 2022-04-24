8b230c372561830c4afb11dd41159034690421a1
Neil Morrison <email@neil.today>
2022-04-24 21:28:17 +0100

    pi -> bluez in another few places

--- lib/settings.ts
+++ lib/settings.ts
@@ -1,2 +1,2 @@
-export const PLATFORM_NAME = "am43-blinds-pi"
-export const PLUGIN_NAME = "homebridge-am43-blinds-pi"
+export const PLATFORM_NAME = "am43-blinds-bluez"
+export const PLUGIN_NAME = "homebridge-am43-blinds-bluez"
